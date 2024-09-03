d42bf92078b9640570007a28ff1cabf919be0634
Alyazid Alhumaydani <alyazid@namla.sa>
2024-09-03 09:21:32 +0300

    scrape

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,13 @@
 <script>
 	import '../app.css';
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
 </script>
 
-
+<Navbar>
+	<NavBrand>
+		<img src="/logo.png" class="me-3 h-6" alt="Company Logo">
+		<span></span>
+	</NavBrand>
+</Navbar>
 
 <slot></slot>
