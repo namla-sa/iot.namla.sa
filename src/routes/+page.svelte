7def081adb951221a31f3dd20056527d3019d65d
Alyazid Alhumaydani <alyazid@namla.sa>
2024-09-03 08:57:31 +0300

    scrape

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<div id="hero">
+    <div>
+        <h1>All Your I.T. Solutions</h1>
+        <p>Out source  <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>   
+    </div>
+</div>
+<div id="details">
+
+    <div>
+        
+    </div>
+</div>
