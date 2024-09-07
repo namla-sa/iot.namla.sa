fe711ed2a08b52aba00f9eef0541692925558c99
Alyazid Alhumaydani <alyazid@namla.sa>
2024-09-07 22:01:42 +0300

    scrape

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,23 +1,31 @@
 <script>
 	import '../app.css';
 	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+	import { page } from '$app/stores';
+	$: activeUrl = $page.url.pathname;
+	let activeClass = 'text-sky-50 bg-cyan-700 md:bg-transparent md:text-cyan-300';
+	let nonActiveClass = 'text-gray-600 hover:bg-gray-300 md:hover:bg-transparent md:border-0 md:hover:text-cyan-600 dark:text-gray-400';
 </script>
 
-<Navbar fluid={true} class=" bg-sky-900 text-sky-100">
-<a href="/"><NavBrand>
-	<img src="https://cdn.namla.sa/namla.png" class="me-3 h-6 rounded-md" alt="Namla Logo">
+<header class="absolute inset-x-0 top-0 z-50">
+<Navbar fluid={true} class=" bg-black shadow-lg shadow-cyan-500/5 text-cyan-200">
+<a href="/">
+	<NavBrand>
+	<img src="logo.png" class="me-2 h-7 rounded-md" alt="Namla Logo">
 	<span class="self-center whitespace-nowrap text-xl font-semibold">namla.sa</span>
 </NavBrand>
 </a>
 
-<NavHamburger/>
-<NavUl>
+<NavHamburger class='hover:bg-black'/>
+<NavUl {activeUrl} {activeClass} {nonActiveClass}>
 	<NavLi href="/">Home</NavLi>
 	<NavLi href="/products">Products</NavLi>
 	<NavLi href="/services">Services</NavLi>
 	<NavLi href="/about">About</NavLi>
 	<NavLi href="/careers">Careers</NavLi>
 </NavUl>
+
 </Navbar>
 
+</header>
 <slot></slot>
--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to Services Page</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="relative isolate px-6 pt-14 lg:px-8">
+
+    <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+        <div class="hidden sm:mb-8 sm:flex sm:justify-center">
+        <div class="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+            Check out our announcements <a href="/research" class="font-semibold text-sky-600"><span class="absolute inset-0" aria-hidden="true"></span>Read more <span aria-hidden="true">&rarr;</span></a>
+        </div>
+        </div>
+        <div class="text-center">
+        <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Data to enrich your online business</h1>
+        <p class="mt-6 text-lg leading-8 text-gray-600">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
+        
+        <div class="mt-10 flex items-center justify-center gap-x-6">
+            <a href="/products" class="rounded-md bg-sky-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600">Our Products</a>
+            <a href="/about" class="text-sm font-semibold leading-6 text-gray-900">Learn more <span aria-hidden="true">→</span></a>
+        </div>
+        </div>
+    </div>
+</div>
